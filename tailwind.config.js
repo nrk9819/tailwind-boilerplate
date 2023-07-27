121c9ca314b1c0604f918e641ec976f0bf29d022
nrk9819 <email@example.com>
2023-07-28 00:45:56 +0530

    add color preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./dist/**/*.html'],
+  presets: [require('./presets/colors.js')],
   theme: {
-    extend: {},
+    spacing: {
+      0: '0px',
+      1: '1px',
+      2: '2px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+      10: '10px',
+      12: '12px',
+      16: '16px',
+      20: '20px',
+      24: '24px',
+      32: '32px',
+      40: '40px',
+      48: '48px',
+      56: '56px',
+      64: '64px',
+      80: '80px',
+      96: '96px',
+      128: '128px',
+      144: '144px',
+      160: '160px',
+      224: '224px',
+    },
   },
   plugins: [],
 };
